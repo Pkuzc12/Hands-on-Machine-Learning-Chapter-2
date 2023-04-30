11aec6d9848108f8c4b9f25894aa92f3db0ed8d2
Pkuzc12 <2000012425@stu.pku.edu.cn>
2023-04-30 16:44:07 +0800

    Paragraphs involving urllib.request altered.

--- FetchData.py
+++ FetchData.py
@@ -1,6 +1,6 @@
 import os
 import tarfile
-import urllib
+import requests
 
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
@@ -11,7 +11,12 @@ HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     os.makedirs(housing_path, exist_ok=True)
     tgz_path = os.path.join(housing_path, "housing.tgz")
-    urllib.request.urlretrieve(housing_url, tgz_path)
+
+    response = requests.get(housing_url)
+    response.raise_for_status()
+    with open(tgz_path, "wb") as f:
+        f.write(response.content)
+
     housing_tgz = tarfile.open(tgz_path)
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
