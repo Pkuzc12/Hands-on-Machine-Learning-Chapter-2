2b55ee152084930f7fb2cd4f91434dabeef23811
Pkuzc12 <2000012425@stu.pku.edu.cn>
2023-05-10 17:30:32 +0800

    Nothing special

--- StratifiedSampling.py
+++ StratifiedSampling.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit
 
 HOUSING_PATH = os.path.join("datasets", "housing")
 
@@ -13,5 +14,12 @@ def load_housing_data(housing_path=HOUSING_PATH):
 
 housing = load_housing_data()
 housing["income_cat"] = pd.cut(housing["median_income"], bins=[0., 1.5, 3.0, 4.5, 6., np.inf], labels=[1, 2, 3, 4, 5])
-housing["income_cat"].hist()
-plt.show()
+# housing["income_cat"].hist()
+# plt.show()
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+# print(strat_test_set["income_cat"].value_counts()/len(strat_test_set))
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
