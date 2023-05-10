fac1b1c56de91adf0afae59c42194bb30a0e219b
Pkuzc12 <2000012425@stu.pku.edu.cn>
2023-05-10 19:25:17 +0800

    Virtual environment

--- StratifiedSampling.py
+++ StratifiedSampling.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import os
-import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import StratifiedShuffleSplit
 
---
+++ VisualizingGeographicalData.py
@@ -0,0 +1,27 @@
+# First, load data; Second, stratified shuffle plit; Third, visualize geographical data.
+
+import os
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit
+import matplotlib.pyplot as plt
+
+HOUSING_PATH = os.path.join("datasets", "housing")
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+
+housing = load_housing_data()
+housing["income_cut"] = pd.cut(housing["median_income"], bins=[0., 1.5, 3.0, 4.5, 6., np.inf], labels=[1, 2, 3, 4, 5])
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cut"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+for set_ in [strat_train_set, strat_test_set]:
+    set_.drop("income_cut", axis=1, inplace=True)
+housing = strat_train_set.copy()
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
+plt.show()
